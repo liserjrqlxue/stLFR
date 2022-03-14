071ea62a7d3478cb9d48e13b7c97bd8251390bd5
liser.jrqlxue <liser.jrqlxue@gmail.com>
2022-03-14 18:44:59 +0800

    style::art:perltidy

--- BIN/filter.pl
+++ BIN/filter.pl
@@ -1,13 +1,14 @@
 use strict;
 use File::Basename qw'basename';
-my $list=shift;
-my $outdir=shift;
+my $list   = shift;
+my $outdir = shift;
 open LIST, $list;
-while(<LIST>){
-	chomp;
-	my $fq1=$_;
-	my $fq2=s/_R1_/_R2_/r;
-	my $outfq1=basename($fq1);
-	my $outfq2=basename($fq2);
-	print "/share/udata/wangyaoshen/src/HPC_chip/tools/SOAPnuke filter -1 $fq1 -2 $fq2 -o $outdir -C $outfq1 -D $outfq2 -G\n";
+while (<LIST>) {
+  chomp;
+  my $fq1    = $_;
+  my $fq2    = s/_R1_/_R2_/r;
+  my $outfq1 = basename($fq1);
+  my $outfq2 = basename($fq2);
+  print
+    "/share/udata/wangyaoshen/src/HPC_chip/tools/SOAPnuke filter -1 $fq1 -2 $fq2 -o $outdir -C $outfq1 -D $outfq2 -G\n";
 }
